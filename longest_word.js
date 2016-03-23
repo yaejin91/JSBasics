c57dfd8f3cca89767a0ffb26ed4e635e5f58f388
Yae Jin Park <yaejin91@gmail.com>
2016-03-22 19:55:05 -0700

    longest_word problem

---
+++ longest_word.js
@@ -0,0 +1,15 @@
+//Using the JavaScript language, have the function LongestWord(sen) take the sen parameter 
+//being passed and return the largest word in the string. 
+//If there are two or more words that are the same length, 
+//return the first word from the string with that length. Ignore punctuation.
+
+function LongestWord(sen) { 
+
+  // code goes here  
+  return sen; 
+         
+}
+   
+// keep this function call here 
+LongestWord(readline());                            
+
