47ea544e40c2ba2c466a1f5b0d536ec61642a064
Yae Jin Park <yaejin91@gmail.com>
2016-03-30 18:37:38 -0700

    check nums solved

---
+++ check_nums.js
@@ -0,0 +1,24 @@
+//Using the JavaScript language, have the function CheckNums(num1,num2) 
+//take both parameters being passed and return the string true if num2 is greater than num1, 
+//otherwise return the string false. 
+//If the parameter values are equal to each other then return the string -1.
+
+function CheckNums(num1,num2) { 
+
+  // code goes here  
+  if(num2 > num1){
+      return true;
+    }
+    
+    if(num2 == num1){
+      return -1;
+    }
+  
+  else {
+      return false;
+    }
+  
+}
+   
+// keep this function call here 
+CheckNums(readline());           
