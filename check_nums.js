dfaea255727cfa74a504cb5a034d76aeec7846c4
Yae Jin Park <yaejin91@gmail.com>
2016-03-30 18:38:30 -0700

    indentation fixed

--- check_nums.js
+++ check_nums.js
@@ -8,15 +8,15 @@ function CheckNums(num1,num2) {
   // code goes here  
   if(num2 > num1){
       return true;
-    }
+  }
     
-    if(num2 == num1){
-      return -1;
-    }
+  if(num2 == num1){
+    return -1;
+  }
   
   else {
       return false;
-    }
+  }
   
 }
    
